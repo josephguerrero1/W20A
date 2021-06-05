10e089ed3f5b2b41c913673bea99494ef052e7da
Joseph Guerrero <joseph.guerrero1@outlook.com>
2021-06-04 23:33:29 -0600

    Created a way for the user to type in their username and have it printed.

---
+++ app.py
@@ -0,0 +1,6 @@
+# Created a variable "username" to store what the user types in as their username.
+# Used the input function so the user can type in their username as a string data type.
+username = input("Enter your username: ")
+
+# Printed the user's username that they typed in.
+print(username)
